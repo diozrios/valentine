9123654f288fb2d03834d9f753563e6a3d4f31ec
diozrios <98732249+diozrios@users.noreply.github.com>
2025-03-23 11:18:39 +0100

    final version completed

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # valentine
-send this to your crush and they will fall on your knees 😉
+Mandalo a crush e si inginocchierà ai tuoi piedi 😉
+
+Copia il link seguente se crush è un RAGAZZO
+```
+https://diozrios.github.io/valentine/?genere=m
+```
+
+Copia il link seguente se crush è una RAGAZZA
+```
+https://diozrios.github.io/valentine/?genere=f
+```
--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+function getParametro(name) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(name);
+}
+
+const genere = getParametro("genere");
 const container = document.getElementById("container");
+
+if (genere === "m") {
+    document.getElementById("domanda").innerHTML = "Vuoi essere il mio Valentino? 💞";
+}
 
 function yes() {
     container.innerHTML = "";
 
     var picture = document.createElement("img");
-    picture.setAttribute("src", "./peach-goma-love.gif");
+    picture.setAttribute("src", "./images/peach-goma-love.gif");
     container.appendChild(picture);
     
     var testo = document.createElement("p");
     testo.innerHTML = "💘🥰❤️‍🔥💞 Daje 😍💖💗💌";
     container.appendChild(testo);
 }
-
 
 function no() {
     var yes = document.getElementById('yes');
@@ -25,6 +34,6 @@ function no() {
     var currentWidth = parseInt(window.getComputedStyle(no).getPropertyValue('width'));
     var currentHeight = parseInt(window.getComputedStyle(no).getPropertyValue('height'));
     
-    no.style.width = (currentWidth - 0.5) + 'px';
-    no.style.height = (currentHeight - 0.5) + 'px';
+    no.style.width = (currentWidth - 0.1) + 'px';
+    no.style.height = (currentHeight - 0.1) + 'px';
 }
